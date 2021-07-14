2021c6d3b5d8dc9379ce149fbdb91ea54d6c8e94
AKANKSHA SHARMA <atmishkumar@gmail.com>
2021-07-13 20:58:54 -0700

    adding some products

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,4 +1,4 @@
-import img1 from '../Assets/Image/img1.jpg'
+import img1 from '../Assets/Image/img1.jpeg'
 import img2 from '../Assets/Image/img2.jpg'
 import img3 from '../Assets/Image/img3.jpg'
 import img4 from '../Assets/Image/img4.webp'
@@ -7,71 +7,110 @@ import img6 from '../Assets/Image/img6.png'
 import img7 from '../Assets/Image/img7.webp'
 import img8 from '../Assets/Image/img8.webp'
 import img9 from '../Assets/Image/img9.jpg'
+import img10 from '../Assets/Image/img10.jpg'
+import img11 from '../Assets/Image/img11.jpg'
+import img12 from '../Assets/Image/img12.jpg'
+import img13 from '../Assets/Image/img13.jpg'
 
 const data = { 
     productData:[
-        {
-           id:1,
-           img: img1,
-           title:"Kiwi",
-           desc: "",
-           price: 46
-        },
-        {
-            id:2,
-            img: img2,
-            title:"Juicy Grapes",
-            desc: "",
-            price: 50
-         },
-         {
-            id:3,
-            img: img3,
-            title:"Juicy Grapes",
-            desc: "",
-            price: 40
-         },
+      {
+         id:1,
+         img: img1,
+         title:"Red Plain Cotton Kids-Frocks",
+         desc: "",
+         price: 60
+      },
+      {
+         id:2,
+         img: img2,
+         title:"Philips Hair Dryer",
+         desc: "",
+         price: 29
+      },
+      {
+         id:3,
+         img: img3,
+         title:"Frocks",
+         desc: "",
+         price: 55
+      },
          {
             id:4,
             img: img4,
-            title:"Juicy Grapes",
+            title:"Formal Dress",
             desc: "",
-            price: 40
+            price: 60
          },
          {
             id:5,
             img: img5,
-            title:"Juicy Grapes",
+            title:"Blazer",
             desc: "",
-            price: 40
+            price: 90
          },
          {
             id:6,
             img: img6,
-            title:"Juicy Grapes",
+            title:"Shoes",
             desc: "",
-            price: 40
+            price: 30
          },
          {
             id:7,
             img: img7,
-            title:"Juicy Grapes",
+            title:"Blazer Shirts",
             desc: "",
             price: 40
          },
          {
             id:8,
             img: img8,
-            title:"Juicy Grapes",
+            title:"Shoes",
             desc: "",
-            price: 40
+            price: 45
          },
          {
             id:9,
             img: img9,
-            title:"Juicy Grapes",
+            title:"Blazer",
             desc: "",
-            price: 40
+            price: 80
+         },
+         {
+            id:10,
+            img: img10,
+            title:"Blue Plain Cotton-Frocks",
+            desc: "",
+            price: 35
+         },
+         {
+            id:10,
+            img: img10,
+            title:"Blue Plain Cotton Kids-Frocks",
+            desc: "",
+            price: 65
+         },
+         {
+            id:11,
+            img: img11,
+            title:"HTC Trimmer",
+            desc: "",
+            price: 39
+         },
+         {
+            id:12,
+            img: img12,
+            title:"Green Cotton-Frocks",
+            desc: "",
+            price: 63
+         },
+         {
+            id:12,
+            img: img13,
+            title:"Philips Trimmer",
+            desc: "",
+            price: 50
          }
     ],
 };
